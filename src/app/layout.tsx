9e9f803c16b28bd638d11c026719bc0a22bcc79b
Michalis Mouzakitis <mouzakitis.m89@gmail.com>
2025-02-18 14:21:48 +0200

    � Add Vercel Analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavbarWrapper from "./components/NavbarWrapper";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className={` ${geistSans.variable} ${geistMono.variable} antialiased`}>
         <NavbarWrapper />
         <main>{children}</main>
+        <Analytics />
       </body>
     </html>
   );
