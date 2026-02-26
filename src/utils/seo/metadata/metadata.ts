36250537f176be91a940198e1f3d88d9c1722a58
Michalis Mouzakitis <mouzakitis.m89@gmail.com>
2026-02-27 01:21:07 +0200

    fix: ios icon for pwa

--- src/utils/seo/metadata/metadata.ts
+++ src/utils/seo/metadata/metadata.ts
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
       { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
-    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+    apple: [{ url: '/icon-maskable-192x192.png', sizes: '192x192', type: 'image/png' }],
   },
 
   manifest: '/manifest.webmanifest',
