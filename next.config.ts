afefa8875103999fdaa9beed8b552de838b347e8
Michalis Mouzakitis <mouzakitis.m89@gmail.com>
2026-04-25 22:05:39 +0300

    fix: reload issue on pwa

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const withPWA = withPWAInit({
   customWorkerSrc: 'src/worker',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  reloadOnOnline: true,
+  reloadOnOnline: false,
   fallbacks: {
     document: '/offline',
   },
